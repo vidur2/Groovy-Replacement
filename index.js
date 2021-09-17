59b5f62f7bbafc0753fcd95a7489a66ae887e1af
vidur2 <vmod2005@gmail.com>
2021-09-17 12:49:07 -0400

    shit

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 
 const youtubedl = require("./node_modules/youtube-dl-exec");
 const got = require("./node_modules/got");
+const { Client, Intents } = require("./node_modules/discord.js")
+
+const TOKEN = "ODg4MTYzNzc1MDE3MDA5MjAy.YUOs-Q.WhlKw1E-gmMQVcZ822E7EDQDSQQ";
+// oauth link: "https://discord.com/api/oauth2/authorize?client_id=888163775017009202&permissions=3160064&scope=bot"
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.GUILDS.VOICE_STATES] });
+client.login(TOKEN)
+let isPlaying = false;
 
 async function getLink(link){
     const finalLink = youtubedl( link, {
@@ -16,14 +24,34 @@ async function getLink(link){
       return finalLink;
 }
 
-const link = "https://www.youtube.com/watch?v=iJ47aOaGvyI";
 
-const linkInfo = getLink(link);
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
 
-linkInfo.then((value) => {
-  const url = value.formats[0].url;
-  console.log(url);
-  const stream = got.stream(url);
-  stream.path = "video.mp4";
+client.on("message", (msg) => {
+  console.log(msg.content)
+  const hasRequiredTag = msg.content.indexOf("-p ")
+  console.log(hasRequiredTag)
+  if(hasRequiredTag != -1 && isPlaying == false){
+    isPlaying = true;
+    const voiceChannel = message.member.voice.channel;
+    const link = msg.content.split(" ");
+    const linkInfo = getLink(link[1]);
+    msg.reply(`Song ${link[1]} has been added to queue`)
+
+    linkInfo.then((value) => {
+      const url = value.formats[0].url;
+      console.log(value);
+      const stream = got.stream(url);
+      stream.path = "music.mp3"
+      voiceChannel.join().then((connection => {
+        const dispatcher = connection.play(stream)
+        dispatcher.on("end", end => {
+          isPlaying = false;
+        })
+      }))
+    })
+  }
 })
 
